c22870dfd13dd38918fd4376c3c0d48771d71bab
Gilberto Bermúdez Garro <gbermude@outlook.com>
2024-09-11 17:07:12 -0600

    Update

--- 20240911/demo7/src/components/product/ProductCreate.tsx
+++ 20240911/demo7/src/components/product/ProductCreate.tsx
@@ -1,10 +1,38 @@
+import { SyntheticEvent, useState } from "react";
+import IProduct from "../../model/IProduct";
+
 function ProductCreate() {
+    const [product, setProduct] = useState<IProduct>({} as IProduct);
+
+    function checkValidation(event: SyntheticEvent) {
+        console.log('checkValidation');
+
+        const target = event.target as HTMLInputElement;
+
+        if (target) {
+            const property = target.name;
+            const value = target.value;
+
+            // const p1 = {productName: 'Chai'};
+            // p1.productName = 'Chai';
+            // p1['productName'] = 'Chai';
+
+            setProduct({...product, ...{[property]: value}});
+        }
+    }
+
   return (
     <>
         <h2>Product</h2>
-        <h3>Create</h3>    
+        <h3>Create</h3>  
         <hr />
+        { JSON.stringify(product) }
         <form>
+            <label htmlFor="">Name</label>            
+            <input type="text" placeholder="Type product name .."
+            name="productName"
+            onChange={event => checkValidation(event) } />
+            <br />
             <button type="submit">Save</button>
         </form>
     </>
