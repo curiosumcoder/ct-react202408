874b457693cd707df6a6f74b552e1115b81fddfc
Gilberto Bermúdez Garro <gbermude@outlook.com>
2024-09-11 16:24:06 -0600

    Update

---
+++ 20240911/demo7/src/App.tsx
@@ -0,0 +1,14 @@
+import ProductCreate from "./components/product/ProductCreate"
+
+function App() {
+
+  return (
+    <>
+      <h1>App</h1>
+      <hr />
+      <ProductCreate></ProductCreate>
+    </>
+  )
+}
+
+export default App
---
+++ 20240911/demo7/src/components/product/ProductCreate.tsx
@@ -0,0 +1,14 @@
+function ProductCreate() {
+  return (
+    <>
+        <h2>Product</h2>
+        <h3>Create</h3>    
+        <hr />
+        <form>
+            <button type="submit">Save</button>
+        </form>
+    </>
+  )
+}
+
+export default ProductCreate
