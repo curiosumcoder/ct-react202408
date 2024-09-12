b481663c6d10696f9e38132eda074d8eb443bab7
Gilberto Bermúdez Garro <gbermude@outlook.com>
2024-09-11 20:13:15 -0600

    Update

--- 20240911/demo8/src/App.tsx
+++ 20240911/demo8/src/App.tsx
@@ -3,10 +3,14 @@ import IProduct from "./model/IProduct";
 import ProductSearch from "./components/product/ProductSearch";
 import ProductList from "./components/product/ProductList";
 import ProductDetail from "./components/product/ProductDetail";
+import AppState from "./model/AppState";
+import ProductCreate from "./components/product/ProductCreate";
 
 function App() {
   const [resultado, setResultado] = useState(Array<IProduct>());
   const [seleccionado, setSeleccionado] = useState<IProduct | null>(null);
+  const [appState, setAppState] = useState<AppState>(AppState.Search);
+
   return (
     <>
       <nav className="navbar bg-light">
@@ -17,18 +21,30 @@ function App() {
         </div>
       </nav>
       <div className="container" style={{ marginTop: "1em" }}>
+        {appState === AppState.Search && (
+          <button
+            className="btn btn-primary"
+            onClick={() => setAppState(AppState.Create)}>
+            Create
+          </button>
+        )}
+        {appState === AppState.Create && <ProductCreate 
+        onCreate={()=> setAppState(AppState.Search)} />}
+        <hr />
         <p>{resultado.length}</p>
-        <ProductSearch onResults={ data => setResultado(data) } ></ProductSearch>
+        <ProductSearch onResults={(data) => setResultado(data)}></ProductSearch>
 
         <div className="row">
           <div className="col-8">
-            <ProductList products={resultado} onSelect={ data => setSeleccionado(data)}></ProductList>
+            <ProductList
+              products={resultado}
+              onSelect={(data) => setSeleccionado(data)}
+            ></ProductList>
           </div>
           <div className="col-4">
-            <ProductDetail p={seleccionado} ></ProductDetail>
+            <ProductDetail p={seleccionado}></ProductDetail>
           </div>
         </div>
-        
       </div>
     </>
   );
---
+++ 20240911/demo8/src/components/product/ProductCreate.tsx
@@ -0,0 +1,82 @@
+import { SyntheticEvent, useRef, useState } from "react";
+import IProduct from "../../model/IProduct";
+import ProductService from "../../services/ProductService";
+
+function ProductCreate({onCreate}: {onCreate?: () => void}) {
+  const [product, setProduct] = useState<IProduct>({} as IProduct);
+  const [isValid, setIsValid] = useState<boolean>(false);
+  const form = useRef<HTMLFormElement>(null);
+
+  const handleSummit = async (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    console.log("Saving product ...");
+    const ps = new ProductService();
+    await ps.save(product);
+    console.log("Product saved!");
+    onCreate?.();
+  };
+
+  function checkValidation(event: SyntheticEvent) {
+    console.log("checkValidation");
+
+    const target = event.target as HTMLInputElement;
+
+    if (target) {
+      setIsValid(form.current?.checkValidity() ?? false);
+
+      if (target.validity.valid) {
+        target.classList.remove("is-invalid");
+        target.classList.add("is-valid");
+
+        const property = target.name;
+        const value = target.value;
+
+        setProduct({ ...product, ...{ [property]: value } });
+      }
+      else {
+        target.classList.remove("is-valid");
+        target.classList.add("is-invalid");
+      }
+    }
+  }
+
+  return (
+    <>
+      <h2>Product</h2>
+      <h3>Create</h3>
+      <hr />
+      {/* {JSON.stringify(product)} */}
+      <form onSubmit={handleSummit} ref={form}>
+        <label htmlFor="" className="form-label">Name</label>
+        <input
+          type="text"
+          placeholder="Type product name .."
+          name="productName"
+          onChange={checkValidation}
+          required
+          minLength={2}
+          maxLength={32}
+          className="form-control"
+        />
+        <br />
+        <label htmlFor="" className="form-label">Unit Price</label>
+        <input
+          type="number"
+          placeholder="Type unit price .."
+          name="unitPrice"
+          onChange={checkValidation}
+          required
+          min={1}
+          className="form-control"
+        />
+        <br />
+        <button type="submit" disabled={!isValid} className="btn btn-light">
+          Save
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default ProductCreate;
